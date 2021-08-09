b1a219f82f005b2240dfa148a4fd689423a28077
Xorol <88636352+Xorol@users.noreply.github.com>
2021-08-08 17:27:15 -0700

    Update deadfish.py

--- Dead fish (><x>)/deadfish.py
+++ Dead fish (><x>)/deadfish.py
@@ -6,9 +6,10 @@ with open('main.deadf') as d:
   for i in d.read(): comm.append(i if i in ['s','o','i','d'] else '')
 comm = ''.join(comm)
 
+#Main loop
 for i in comm:
   if i == 's': stack[cell] *= stack[cell]
   elif i == 'd': stack[cell] -= 1
   elif i == 'i': stack[cell] += 1
   elif i == 'o': print(stack[cell])
-  else: continue
+  if control == 256 or control == -1: control = 0
