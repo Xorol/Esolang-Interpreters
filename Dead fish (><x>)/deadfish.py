b3363a3e4ca69d2085e0b8b576d430e07a49c8cc
Xorol <88636352+Xorol@users.noreply.github.com>
2021-08-08 17:22:08 -0700

    Create deadfish.py

---
+++ Dead fish (><x>)/deadfish.py
@@ -0,0 +1,14 @@
+control = 0 #initialize the accumulator
+
+#open the source file, get the relevent chars, and store in comm
+comm = []
+with open('main.deadf') as d:
+  for i in d.read(): comm.append(i if i in ['s','o','i','d'] else '')
+comm = ''.join(comm)
+
+for i in comm:
+  if i == 's': stack[cell] *= stack[cell]
+  elif i == 'd': stack[cell] -= 1
+  elif i == 'i': stack[cell] += 1
+  elif i == 'o': print(stack[cell])
+  else: continue
