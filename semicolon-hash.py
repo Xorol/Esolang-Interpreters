aecefc1cf9156f0f0a75606b0b9bb7c93a6a57c5
Xorol <88636352+Xorol@users.noreply.github.com>
2021-08-08 17:06:07 -0700

    Added check for illegal characters

--- semicolon-hash.py
+++ semicolon-hash.py
@@ -4,6 +4,7 @@ control = 0
 while True:
   code = input('>> ')
   for i in code:
+    if i not in [';','#']: continue
     control = control + 1 if i == ';' else chr(control % 127)
     if i == '#':
       print(control)
