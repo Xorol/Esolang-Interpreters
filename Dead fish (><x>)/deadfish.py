ae065841dc581a0529ba6995895c76f55b431adc
Xorol <88636352+Xorol@users.noreply.github.com>
2021-08-09 16:58:44 -0700

    Update deadfish.py

--- Dead fish (><x>)/deadfish.py
+++ Dead fish (><x>)/deadfish.py
@@ -1,15 +1,12 @@
 control = 0 #initialize the accumulator
 
 #open the source file, get the relevent chars, and store in comm
-comm = []
-with open('main.deadf') as d:
-  for i in d.read(): comm.append(i if i in ['s','o','i','d'] else '')
-comm = ''.join(comm)
-
+d = open('main.deadf','r')
 #Main loop
-for i in comm:
+for i in [i for i in d.read() if i in ['s','o','i','d'] else '']:
   if i == 's': stack[cell] *= stack[cell]
   elif i == 'd': stack[cell] -= 1
   elif i == 'i': stack[cell] += 1
   elif i == 'o': print(stack[cell])
   if control == 256 or control == -1: control = 0
+d.close()
